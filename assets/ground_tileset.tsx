f24fc51cc1632f6116af676bcce7d0115792b02f
dinoplane <isacsac2017@gmail.com>
2022-05-11 09:13:22 -0700

    added for guide

--- assets/ground_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="ground_tileset" tilewidth="32" tileheight="32" tilecount="50" columns="10">
- <image source="ground_tileset.png" width="320" height="160"/>
-</tileset>
