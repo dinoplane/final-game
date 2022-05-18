15dc1d2435ac0c641b010ac1e739d9dc9c435541
Will-Tate <103454866+Will-Tate@users.noreply.github.com>
2022-05-18 15:20:11 -0700

    updated levels and cats

--- assets/tilesets/cats.tsx
+++ assets/tilesets/cats.tsx
@@ -1,38 +1,66 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="cats" tilewidth="228" tileheight="351" tilecount="10" columns="0">
+<tileset version="1.8" tiledversion="1.8.5" name="cats" tilewidth="228" tileheight="351" tilecount="10" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="biscuit">
+  <properties>
+   <property name="movesLeft" type="int" value="1"/>
+  </properties>
   <image width="215" height="53" source="cats_tileset/biscuit.png"/>
  </tile>
  <tile id="1" type="clyde">
+  <properties>
+   <property name="movesLeft" type="int" value="1"/>
+  </properties>
   <image width="152" height="60" source="cats_tileset/clyde.png"/>
  </tile>
  <tile id="3" type="horton">
+  <properties>
+   <property name="movesLeft" type="int" value="1"/>
+  </properties>
   <image width="228" height="168" source="cats_tileset/horton.png"/>
  </tile>
  <tile id="4" type="hubert">
   <properties>
    <property name="end_scale" type="float" value="1"/>
    <property name="init_scale" type="float" value="1"/>
+   <property name="movesLeft" type="int" value="1"/>
   </properties>
   <image width="83" height="351" source="cats_tileset/hubert.png"/>
  </tile>
  <tile id="5" type="jasmine">
+  <properties>
+   <property name="movesLeft" type="int" value="1"/>
+  </properties>
   <image width="102" height="189" source="cats_tileset/jasmine.png"/>
  </tile>
  <tile id="6" type="luna">
+  <properties>
+   <property name="movesLeft" type="int" value="0"/>
+  </properties>
   <image width="132" height="65" source="cats_tileset/luna.png"/>
  </tile>
  <tile id="7" type="newton">
+  <properties>
+   <property name="movesLeft" type="int" value="1"/>
+  </properties>
   <image width="157" height="96" source="cats_tileset/newton.png"/>
  </tile>
  <tile id="8" type="quinn">
+  <properties>
+   <property name="movesLeft" type="int" value="1"/>
+  </properties>
   <image width="127" height="85" source="cats_tileset/quinn.png"/>
  </tile>
  <tile id="2" type="davinci">
+  <properties>
+   <property name="movesLeft" type="int" value="1"/>
+  </properties>
   <image width="58" height="129" source="cats_tileset/davinci.png"/>
  </tile>
  <tile id="9" type="dreda">
+  <properties>
+   <property name="movesLeft" type="int" value="1"/>
+  </properties>
   <image width="204" height="50" source="cats_tileset/dreda.png"/>
  </tile>
 </tileset>
