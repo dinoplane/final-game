d0d2bfd6f3749edb3e95b9f1ff5c9131abc3ccef
dinoplane <isacsac2017@gmail.com>
2022-05-18 10:03:50 -0700

    uwu, i fixed the selection

--- src/prefab/Cat.js
+++ src/prefab/Cat.js
@@ -1,5 +1,8 @@
 class Cat extends Phaser.Physics.Arcade.Sprite {
-    static SELECTED_CAT = null;
+    static P2C_COLLIDER = null;
+    static P2C_OVERLAP = null;
+    static C2C_OVERLAP = null;
+    
 
     constructor(scene, x, y, frame, data){
         super(scene, x, y, "cats_atlas", frame);
@@ -7,16 +10,48 @@ class Cat extends Phaser.Physics.Arcade.Sprite {
         scene.add.existing(this);
         scene.physics.add.existing(this);
         this.addPointerCallbacks();
+        this.debugtext = scene.add.text(x, y, "YOUR MOTHER");
 
         this.name = frame;
         this.selected = false;
+        this.selectsLeft = 1;
+        this.catSoul = null;
+        this.my_friend = null;
     }
+
+//Cat
+// when clicked create a cat soul
+
+// cat soul follows pointer as we drag
+
+// when the drag ends check  if the cat soul is in a valid position
+
+// If so move and decrement selects
+// If not move back and act like that never happened.
+
 
     addPointerCallbacks(){
         this.setInteractive();
-        this.on('pointerdown', (pointer) => {
-            console.log(pointer.x, pointer.y);
-            this.onLeftClicked();
+        // this.on('pointerdown', (pointer) => {
+        //     console.log(pointer.x, pointer.y);
+        //     this.onDragStart();
+        // });
+        this.scene.input.setDraggable(this.setInteractive());
+
+        this.on('dragstart', (pointer, dragX, dragY) => {
+            console.log("HELLO!");
+            
+            this.onDragStart()
+        });
+
+        this.on('drag', (pointer, dragX, dragY) => {
+            this.whileDrag(dragX, dragY);
+        });
+
+        this.on('dragend', (pointer, dragX, dragY) => {
+            console.log("END");
+            this.onDragEnd();
+            
         });
     }
 
@@ -28,32 +63,77 @@ class Cat extends Phaser.Physics.Arcade.Sprite {
 
     }
 
-    onLeftClicked(){
-        console.log (this.selected)
-        if (!this.selected && Cat.SELECTED_CAT == null){
-            this.onSelected();
-        } else if (this.selected && Cat.SELECTED_CAT != null){
-            this.onDeselected()
-        };
+    onCatOverlap(that){ // No pun intended no pun intended
+        this.my_friend = that;  // Cats are friendly to each other
+        that.setTexture("cats_atlas", that.name+"_x3");
+        this.setTexture("cats_atlas", this.name+"_x3");
     }
 
-    onRightClicked(){
-
+    isTouching(){
+        return this.body.embedded || !this.body.touching.none ;
     }
 
-    onSelected(){
-        this.selected = true;
-        Cat.SELECTED_CAT = this;
-        this.setTexture("cats_atlas", this.name+"_owo");
-        // Create a sprite after image. add a collider that switches its animations. 
-
+    isSelectable(){
+        return this.selectsLeft > 0;
     }
 
-    onDeselected(){
-        Cat.SELECTED_CAT = null;
-        this.setTexture("cats_atlas", this.name);
-        this.selected = false;
-        console.log(this.x, this.y)
+    onDragStart(){
+        if (this.isSelectable() && !this.isTouching() && !this.selected){
+            let i = Cat.P2C_COLLIDER.object2.indexOf(this);
+            let j = Cat.P2C_OVERLAP.object2.indexOf(this);
+                        
+
+            Cat.P2C_COLLIDER.object2.splice(i,1);
+            Cat.P2C_OVERLAP.object2.splice(j,1);
+            
+            //Cat.P2C_OVERLAP.object2.remove(this);
+
+            
+            this.catSoul = new CatSoul(this).setOrigin(0,1);
+            this.setTexture("cats_atlas", this.name+"_owo");
+            this.selected = true;
+            this.setScale(1.2);
+            this.setDepth(4);
+
+        }
+    }
+
+    whileDrag(dragX, dragY){
+        if (this.selected){   
+            if (this.body.wasTouching && !this.isTouching()){
+                this.setTexture("cats_atlas", this.name+"_owo");
+                let friend_state = (this.my_friend.isSelectable()) ? "" : "_uwu";
+                this.my_friend.setTexture("cat_atlas", this.my_friend.name + friend_state);
+            }
+            this.debugtext.text = (this.body.embedded || !this.body.touching.none) ? "touching" : "alone"  ;
+            this.x = dragX;
+            this.y = dragY;
+        }
+    }
+
+    onDragEnd(){
+        if (this.selected){  
+            if (this.isTouching()){
+                this.setPosition(this.catSoul.x, this.catSoul.y);
+                this.setTexture("cats_atlas", this.name);
+            }
+            else {
+                this.decrementSelect();
+            }
+            Cat.P2C_COLLIDER.object2.push(this);
+            Cat.P2C_OVERLAP.object2.push(this);
+            this.catSoul.destroy();
+            this.selected = false;
+            this.setScale(1);
+            this.setDepth(0);
+
+        }
+    }
+
+    decrementSelect(){
+        this.selectsLeft -= 1;
+        if (!this.isSelectable) this.setTexture("cat_atlas", this.my_friend.name + "_uwu");
+        else this.setTexture("cats_atlas", this.name);
     }
 
     update(){
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -26,16 +26,21 @@ class Play extends Phaser.Scene {
         });
         this.foodNum = this.objects.food.length;
 
-        this.physics.add.collider(this.player, this.objects.cats, (player, cat) => {
+        Cat.P2C_COLLIDER = this.physics.add.collider(this.player, this.objects.cats.slice(), (player, cat) => {
             cat.onCollide(player);
         });
-        this.physics.add.overlap(this.player, this.objects.cats, (player, cat) => {
+        Cat.P2C_OVERLAP = this.physics.add.overlap(this.player, this.objects.cats.slice(), (player, cat) => {
             cat.onOverlap(player);
+        });
+        
+        Cat.C2C_OVERLAP = this.physics.add.collider(this.objects.cats.slice(), this.objects.cats.slice(), (cat1, cat2) => {
+            cat1.onCatOverlap(cat2);
         });
         
         
         this.input.on("pointerdown", (pointer, obj) => {
             if (Cat.SELECTED_CAT != null && obj.length == 0){
+                Cat.SELECTED_CAT.decrementSelect();
                 Cat.SELECTED_CAT.setPosition(pointer.worldX - Cat.SELECTED_CAT.width/2, pointer.worldY + Cat.SELECTED_CAT.height/2);
                 Cat.SELECTED_CAT.onDeselected();
             }
