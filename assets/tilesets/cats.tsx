eadacc0a066893fe0b1a3de2f9eaabbe9d71546c
dinoplane <isacsac2017@gmail.com>
2022-05-25 16:09:57 -0700

    modified and 2 movesleft

--- assets/tilesets/cats.tsx
+++ assets/tilesets/cats.tsx
@@ -3,19 +3,19 @@
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="biscuit">
   <properties>
-   <property name="movesLeft" type="int" value="1"/>
+   <property name="movesLeft" type="int" value="2"/>
   </properties>
   <image width="215" height="53" source="cats_tileset/biscuit.png"/>
  </tile>
  <tile id="1" type="clyde">
   <properties>
-   <property name="movesLeft" type="int" value="1"/>
+   <property name="movesLeft" type="int" value="2"/>
   </properties>
   <image width="152" height="60" source="cats_tileset/clyde.png"/>
  </tile>
  <tile id="3" type="horton">
   <properties>
-   <property name="movesLeft" type="int" value="1"/>
+   <property name="movesLeft" type="int" value="2"/>
   </properties>
   <image width="228" height="168" source="cats_tileset/horton.png"/>
  </tile>
@@ -23,13 +23,13 @@
   <properties>
    <property name="end_scale" type="float" value="1"/>
    <property name="init_scale" type="float" value="1"/>
-   <property name="movesLeft" type="int" value="1"/>
+   <property name="movesLeft" type="int" value="2"/>
   </properties>
   <image width="83" height="351" source="cats_tileset/hubert.png"/>
  </tile>
  <tile id="5" type="jasmine">
   <properties>
-   <property name="movesLeft" type="int" value="1"/>
+   <property name="movesLeft" type="int" value="2"/>
   </properties>
   <image width="102" height="189" source="cats_tileset/jasmine.png"/>
  </tile>
@@ -41,25 +41,25 @@
  </tile>
  <tile id="7" type="newton">
   <properties>
-   <property name="movesLeft" type="int" value="1"/>
+   <property name="movesLeft" type="int" value="2"/>
   </properties>
   <image width="157" height="96" source="cats_tileset/newton.png"/>
  </tile>
  <tile id="8" type="quinn">
   <properties>
-   <property name="movesLeft" type="int" value="1"/>
+   <property name="movesLeft" type="int" value="2"/>
   </properties>
   <image width="127" height="85" source="cats_tileset/quinn.png"/>
  </tile>
  <tile id="2" type="davinci">
   <properties>
-   <property name="movesLeft" type="int" value="1"/>
+   <property name="movesLeft" type="int" value="0"/>
   </properties>
   <image width="58" height="129" source="cats_tileset/davinci.png"/>
  </tile>
  <tile id="9" type="dreda">
   <properties>
-   <property name="movesLeft" type="int" value="1"/>
+   <property name="movesLeft" type="int" value="2"/>
   </properties>
   <image width="204" height="50" source="cats_tileset/dreda.png"/>
  </tile>
