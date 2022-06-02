7065b2ffc1845837231e1ab8fdd32466beb99999
dinoplane <isacsac2017@gmail.com>
2022-06-02 09:36:10 -0700

    finalized the tutorials. Now to make sure each level got a title

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ var config = {
           x: 0,
           y: 1200
            },  
-        debug: false
+        debug: true
       }
     }
   }
@@ -43,7 +43,7 @@ let cursors;
 let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
-let level = 3;
+let level = 4;
 let controls;
 let pointer;
 let keyF, keyR, keyLEFT, keyRIGHT, keySPACE, keyUP, keyENTER;
